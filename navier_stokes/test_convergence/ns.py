59cddbd7ad63a3f0590e74b78217514fcc85a994
Frostyant <webs.anthony@gmail.com>
2019-03-29 12:17:01 +0000

    fixed stokes test

--- navier_stokes/test_convergence/ns.py
+++ navier_stokes/test_convergence/ns.py
@@ -18,7 +18,7 @@ for it,n in enumerate(Ns):
     V = FunctionSpace(mesh, "BDM", 2)
     Q = FunctionSpace(mesh, "DG", 1)
     W = V * Q
-    u_0 = as_vector([exp(x*y),exp(x*y)])
+    u_0 = as_vector([cos(y)*exp(x),-sin(y)*exp(x)])
 
     problem = rins.rinsp(mesh,u_0,W,x,y,viscosity = mu,BcIds = (1,2,3,4),AdvectionSwitchStep = 0.25,AverageVelocity = AverageVelocity,LengthScale = 1)
     problem.FullSolve(FullOutput = False,DisplayInfo = False,stokes = True)
