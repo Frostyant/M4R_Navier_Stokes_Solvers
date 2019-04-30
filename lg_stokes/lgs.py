072c27095effae4559cd697125aa02746dec443a
Frostyant <webs.anthony@gmail.com>
2019-04-30 14:25:40 +0200

    add lgs forcing

--- lg_stokes/lgs.py
+++ lg_stokes/lgs.py
@@ -11,6 +11,18 @@ W = V*Q
 
 #setting up boundary conditions
 u_0 = as_vector([cos(y)*exp(x),-sin(y)*exp(x)])
+p_0 = Constant(1)*sin(2*pi*x)**2*sin(2*pi*y)**2
+p_x = p_0.dx(0)
+p_y = p_0.dx(1)
+u_x = u_0.dx(0)
+u_y = u_0.dx(1)
+u_xx = u_x.dx(0)
+u_yy = u_x.dx(1)
+F_ = as_vector([ - mu*(u_xx[0]+u_yy[0])
+            , - mu*(u_xx[1] + u_yy[1])])
+F = Function(V)
+F.project(F_)
+
 bc0 = DirichletBC(W.sub(0), u_0, 1)
 bc1 = DirichletBC(W.sub(0), u_0, 2)
 bc2 = DirichletBC(W.sub(0), u_0, 3)
@@ -29,7 +41,7 @@ nullspace = MixedVectorSpaceBasis(
 #setting up problem proper
 LHS = inner(grad(u), grad(v)) * dx + div(v) * p * dx + q * div(u) * dx
 Zero = Function(Q)
-RHS =  q*Zero * dx
+RHS =  inner(v,F) * dx #q*Zero * dx
 
 # Form for use in constructing preconditioner matrix
 aP = inner(grad(u), grad(v))*dx + p*q*dx
