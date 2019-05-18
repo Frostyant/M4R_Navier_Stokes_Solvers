0a783b8237f113e1fac0205dce5bec1abdc9fd64
Frostyant <webs.anthony@gmail.com>
2019-05-19 00:34:47 +0100

    testing if advection forcing works

--- navier_stokes/rins/rins.py
+++ navier_stokes/rins/rins.py
@@ -259,7 +259,7 @@ class rinsp:
 
         #Assembling Advection Term
         adv_byparts1 = inner(u, curl(cross(u, self.v)))*dx #This is the term from integration by parts of double curl
-        adv_byparts2 = inner(U_upwind, jump( perp(n, cross(u, self.v))))*dS #Second term over surface
+        adv_byparts2 = inner(U_upwind, 2*avg( perp(n, cross(u, self.v))))*dS #Second term over surface
         adv_grad_parts1 = 0.5*div(self.v)*inner(u,u)*dx #This is the term due to the integration by parts of grad u^2
         adv_bdc1 = inner(self.u_0,perp(n,cross(self.u_0,self.v)))*ds #boundary version of adv_byparts2
         adv_grad_parts2 = 1/2*inner(inner(self.u_0,self.u_0)*self.v,n)*ds #boundary term from grad u^2 integration by parts
@@ -267,8 +267,8 @@ class rinsp:
             adv_byparts1
             - adv_byparts2
             - adv_grad_parts1
-            - adv_bdc1
-            + adv_grad_parts2
+            #- adv_bdc1
+            #+ adv_grad_parts2
         )
 
         return advection_term
